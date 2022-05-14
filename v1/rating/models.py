d7b044f2acdf566aae438eec82e4b60b140a3549
rico <ricog@web.de>
2022-05-14 22:34:37 +0200

    add timestamps to ratings (close #3)

--- v1/rating/models.py
+++ v1/rating/models.py
@@ -17,11 +17,15 @@ class Rating(models.Model):
     :recipe: = The recipe the comment is related to
     :comment: = A comment on the recipe
     :rating: = A rating 1-5
+    :pub_date: = When the rating was created
+    :update_date: = When the rating was updated
     """
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     comment = models.CharField(_('comment'), max_length=250)
     rating = models.IntegerField(_('rating'), default=0)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '%s - %s' % (self.rating, self.comment)
