048691a9c8ebb934e012bef60834b5dadbb9d2b1
Sarajiko <80350428+Sarajiko@users.noreply.github.com>
2022-07-12 20:03:50 +0200

    Add filters to mini-browser
    This commit will extend the mini-browser endpoint.
    It will add filters for course(__slug) and cuisine(__slug).
    
    See: https://github.com/ownrecipes/OwnRecipes/issues/11
    
    Co-authored-by: rico <ricog@web.de>

--- v1/recipe/views.py
+++ v1/recipe/views.py
@@ -93,6 +93,25 @@ class MiniBrowseViewSet(viewsets.mixins.ListModelMixin,
         else:
             qs = Recipe.objects.filter(public=True)
 
+        filter_set = {}
+        if 'cuisine__slug' in self.request.query_params:
+            filter_set['cuisine__in'] = Cuisine.objects.filter(
+                slug__in=self.request.query_params.get('cuisine__slug').split(',')
+            )
+        if 'course__slug' in self.request.query_params:
+            filter_set['course__in'] = Course.objects.filter(
+                slug__in=self.request.query_params.get('course__slug').split(',')
+            )
+        if 'tag__slug' in self.request.query_params:
+            # filter tags with OR
+            filter_set['tags__in'] = Tag.objects.filter(
+                slug__in=self.request.query_params.get('tag__slug').split(',')
+            )
+            # filter tags with AND (for future use)
+            # tags = self.request.query_params.get('tag__slug').split(',')
+            # for t in tags:
+            #     qs = qs.filter(tags__in=Tag.objects.filter(slug=t))
+        qs = qs.filter(**filter_set)
         # Get the limit from the request and the count from the DB.
         # Compare to make sure you aren't accessing more than possible.
         limit = int(request.query_params.get('limit', 4))
