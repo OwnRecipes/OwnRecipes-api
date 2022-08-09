869f4268639c80735005f6ac9b863b20f98a650b
rico <ricog@web.de>
2022-08-09 21:38:47 +0200

    Unused (old) images are not being cleaned up #14 - shortened test data

--- v1/recipe/tests/test_update_recipe.py
+++ v1/recipe/tests/test_update_recipe.py
@@ -283,113 +283,15 @@ class RecipeSerializerTests(TestCase):
                     "id": 3,
                     "title": "",
                     "ingredients": []
-                },
-                {
-                    "id": 4,
-                    "title": "Veges",
-                    "ingredients": [
-                        {
-                            "id": 13,
-                            "numerator": 1.0,
-                            "denominator": 2.0,
-                            "measurement": "dash",
-                            "title": "black pepper"
-                        },
-                        {
-                            "id": 14,
-                            "numerator": 4.0,
-                            "denominator": 1.0,
-                            "measurement": "tablespoons",
-                            "title": "chili powder"
-                        },
-                        {
-                            "id": 15,
-                            "numerator": 1.0,
-                            "denominator": 1.0,
-                            "measurement": "tablespoon",
-                            "title": "cumin"
-                        },
-                        {
-                            "id": 16,
-                            "numerator": 1.0,
-                            "denominator": 1.0,
-                            "measurement": "can",
-                            "title": "dark kidney beans"
-                        },
-                        {
-                            "id": 17,
-                            "numerator": 2.0,
-                            "denominator": 1.0,
-                            "measurement": "cans",
-                            "title": "diced tomatos"
-                        },
-                        {
-                            "id": 18,
-                            "numerator": 1.0,
-                            "denominator": 1.0,
-                            "measurement": "whole",
-                            "title": "green bell pepper"
-                        },
-                        {
-                            "id": 19,
-                            "numerator": 1.0,
-                            "denominator": 1.0,
-                            "measurement": "can",
-                            "title": "light kidney beans"
-                        },
-                        {
-                            "id": 20,
-                            "numerator": 1.0,
-                            "denominator": 1.0,
-                            "measurement": "whole",
-                            "title": "serrano pepper"
-                        },
-                        {
-                            "id": 21,
-                            "numerator": 1.0,
-                            "denominator": 1.0,
-                            "measurement": "whole",
-                            "title": "white onion"
-                        }
-                    ]
-                },
-                {
-                    "id": 5,
-                    "title": "Beef",
-                    "ingredients": [
-                        {
-                            "id": 22,
-                            "numerator": 1.0,
-                            "denominator": 1.0,
-                            "measurement": "pound",
-                            "title": "ground pork"
-                        },
-                        {
-                            "id": 23,
-                            "numerator": 1.0,
-                            "denominator": 1.0,
-                            "measurement": "pound",
-                            "title": "ground sirloin"
-                        },
-                        {
-                            "id": 24,
-                            "numerator": 1.0,
-                            "denominator": 1.0,
-                            "measurement": "dash",
-                            "title": "kosher salt"
-                        }
-                    ]
                 }
             ],
             "directions": 'test',
-            "tags": [{'title': 'hi'}, {'title': 'hello'}],
             "title": "Recipe name",
             "info": "Recipe info rgo",
             "source": "google.com",
             "prep_time": 60,
             "cook_time": 60,
             "servings": 8,
-            "rating": 0,
             "cuisine": {"id": 1},
             "course": {"id": 2},
             "photo": "upload/recipe_photos/food.jpg"
@@ -401,113 +303,15 @@ class RecipeSerializerTests(TestCase):
                     "id": 3,
                     "title": "",
                     "ingredients": []
-                },
-                {
-                    "id": 4,
-                    "title": "Veges",
-                    "ingredients": [
-                        {
-                            "id": 13,
-                            "numerator": 1.0,
-                            "denominator": 2.0,
-                            "measurement": "dash",
-                            "title": "black pepper"
-                        },
-                        {
-                            "id": 14,
-                            "numerator": 4.0,
-                            "denominator": 1.0,
-                            "measurement": "tablespoons",
-                            "title": "chili powder"
-                        },
-                        {
-                            "id": 15,
-                            "numerator": 1.0,
-                            "denominator": 1.0,
-                            "measurement": "tablespoon",
-                            "title": "cumin"
-                        },
-                        {
-                            "id": 16,
-                            "numerator": 1.0,
-                            "denominator": 1.0,
-                            "measurement": "can",
-                            "title": "dark kidney beans"
-                        },
-                        {
-                            "id": 17,
-                            "numerator": 2.0,
-                            "denominator": 1.0,
-                            "measurement": "cans",
-                            "title": "diced tomatos"
-                        },
-                        {
-                            "id": 18,
-                            "numerator": 1.0,
-                            "denominator": 1.0,
-                            "measurement": "whole",
-                            "title": "green bell pepper"
-                        },
-                        {
-                            "id": 19,
-                            "numerator": 1.0,
-                            "denominator": 1.0,
-                            "measurement": "can",
-                            "title": "light kidney beans"
-                        },
-                        {
-                            "id": 20,
-                            "numerator": 1.0,
-                            "denominator": 1.0,
-                            "measurement": "whole",
-                            "title": "serrano pepper"
-                        },
-                        {
-                            "id": 21,
-                            "numerator": 1.0,
-                            "denominator": 1.0,
-                            "measurement": "whole",
-                            "title": "white onion"
-                        }
-                    ]
-                },
-                {
-                    "id": 5,
-                    "title": "Beef",
-                    "ingredients": [
-                        {
-                            "id": 22,
-                            "numerator": 1.0,
-                            "denominator": 1.0,
-                            "measurement": "pound",
-                            "title": "ground pork"
-                        },
-                        {
-                            "id": 23,
-                            "numerator": 1.0,
-                            "denominator": 1.0,
-                            "measurement": "pound",
-                            "title": "ground sirloin"
-                        },
-                        {
-                            "id": 24,
-                            "numerator": 1.0,
-                            "denominator": 1.0,
-                            "measurement": "dash",
-                            "title": "kosher salt"
-                        }
-                    ]
                 }
             ],
             "directions": 'test',
-            "tags": [{'title': 'hi'}, {'title': 'hello'}],
             "title": "Recipe name",
             "info": "Recipe info rgo",
             "source": "google.com",
             "prep_time": 60,
             "cook_time": 60,
             "servings": 8,
-            "rating": 0,
             "cuisine": {"id": 1},
             "course": {"id": 2},
             "photo": "upload/recipe_photos/food2.jpg"
