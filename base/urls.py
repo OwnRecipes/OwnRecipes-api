56d00f50d23e67ae22c3970497b1e280fafd5102
sepulzera <hartung_spam@gmx.de>
2022-11-18 09:50:54 +0100

    #78 ADMIN_URL setting with trailing slash

--- base/urls.py
+++ base/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
 
     # Admin
-    path(f'{settings.ADMIN_URL}/', admin.site.urls),
+    path(settings.ADMIN_URL, admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
