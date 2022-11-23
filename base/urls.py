e385e3743991094563cda117a8f23eade1493ba5
sepulzera <hartung_spam@gmx.de>
2022-11-23 22:27:05 +0100

    Release 3.1.1

--- base/urls.py
+++ base/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('openapi', get_schema_view(
         title='OwnRecipes',
         description='OwnRecipes OpenAPI',
-        version='3.1.0',
+        version='3.1.1',
     ), name='openapi'),
 
     # Backend REST API
